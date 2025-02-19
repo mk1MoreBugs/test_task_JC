a456377fc42aa53418b06dfc437582f15a855714
mk1MoreBugs <o.v.klevtsov@ya.ru>
2025-02-19 23:15:47 +0300

    fix crud def update_wallet_amount_by_uuid()

--- backend/app/crud/wallets.py
+++ backend/app/crud/wallets.py
@@ -1,6 +1,6 @@
 from uuid import UUID
 
-from sqlalchemy import Select, update
+from sqlalchemy import update
 from sqlmodel import Session, select
 
 from app.models.wallets import Wallet
@@ -12,11 +12,14 @@ def create_wallet(session: Session) -> Wallet:
     session.commit()
     return wallet
 
+
 def read_wallet_by_uuid(session: Session, wallet_uuid: UUID) -> Wallet | None:
-    statement: Select = select(Wallet).where(Wallet.uuid == wallet_uuid)
+    statement = select(Wallet).where(Wallet.uuid == wallet_uuid)
     wallet: Wallet | None = session.exec(statement).first()
     return wallet
+
 
 def update_wallet_amount_by_uuid(session: Session, wallet_uuid: UUID, amount: int) -> None:
     statement = update(Wallet).where(Wallet.uuid == wallet_uuid).values(amount=amount)
     session.exec(statement)
+    session.commit()
